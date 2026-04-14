e7db99e11e8963d0c1fc92e240fd09823fc78b06
demodogo <macaigm@gmail.com>
2026-04-13 20:52:30 -0400

    remove unused import in MediaController

--- src/main/java/com/duoc/recetas/controller/MediaController.java
+++ src/main/java/com/duoc/recetas/controller/MediaController.java
@@ -1,6 +1,5 @@
 package com.duoc.recetas.controller;
 
-import org.apache.tomcat.util.http.parser.MediaType;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
